933cd97351e0455672097a8f17b97fe97d01363f
mxfu <mphu@stevens.edu>
2022-04-05 22:10:22 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+  return <viewImageScreen/>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ app/assets/screens/viewItemScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+function viewImageScreen(props){
+    return(
+        <div>
+            return(
+                <Image source={require('.../assets/computer mouse.jpg')}/>;
+            )
+        </div>
+    );
+}
+
+export default viewImageScreen;
