c4bb0785f96bc371bbf188bdc4d92f9290d2573c
mxfu <mphu@stevens.edu>
2022-04-06 00:20:51 -0400

    updated the View Item Screen to have the image

--- app/assets/screens/viewItemScreen.js
+++ app/assets/screens/viewItemScreen.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 
-function viewImageScreen(props){
+function ViewImageScreen(props){
     return(
-        <div>
-            return(
-                <Image source={require('.../assets/computer mouse.jpg')}/>;
-            )
-        </div>
+        <View style={styles.container}> 
+        <Image 
+        resizeMode = "contain" 
+        style={styles.image} 
+        source={require(".../assets/computer mouse.jpg")}
+    />
+    </View>
     );
 }
 
-export default viewImageScreen;
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: "#000",
+        flex: 1,
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
+export default ViewImageScreen;
